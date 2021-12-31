c609b92867b3cea5858570caaa6884ba3be11b86
Дмитрий <shvdm74@gmail.com>
2021-12-31 21:59:19 +0300

    Добавил новый проект

---
+++ b.js
@@ -0,0 +1,7 @@
+let a=6;
+alert(a);
+var a=7;
+alert (a);
+const d=9;
+
+alert (d);
---
+++ Новая папка/test.js
@@ -0,0 +1,37 @@
+var a='Здавствуйте!';
+alert (a);
+function mnda(c,d) {
+var z=c+"  "+d;
+alert(z);
+}
+var c='Петя';var d='Успенский';
+var z=mnda(c,d);
+
+var c='Артем'; var d='Сергеевич'
+mnda(c,d);
+var c='Олег'; var d='Дмитриевич'
+mnda(c,d);
+var c='Константин'; var d='Васильевич'
+mnda(c,d);
+var c='Пузырь'; var d='Хлюпикович'
+mnda(c,d);
+var c='Артем'; var d='Сергеевич'
+mnda(c,d);
+
+
+/*function mnda(c,d) {
+var z=c+"  "+d;
+return z;
+}
+var c='Петя';var d='Успенский';
+var z=mnda(c,d);
+alert(z);
+
+/*var c='Артем'; var d='Сергеевич';
+var z=mnda(c,d);
+alert (z);
+
+
+var z=mnda('Олег','Дмитриевич');
+var z=mnda('Константин','Васильевич');
+var z=mnda('Пузырь','Хлюпикович');*/
